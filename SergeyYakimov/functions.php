6ebbda978f7eb2a4e3d6d2cc6dd4b4aff347aa73
SergeyYakimov <teriertis@yandex.ru>
2019-10-01 02:35:08 +0800

    Added templates header.php and footer.php

--- SergeyYakimov/functions.php
+++ SergeyYakimov/functions.php
@@ -3,6 +3,7 @@
 add_action('wp_enqueue_scripts', 'style_theme');
 add_action( 'wp_enqueue_scripts', 'my_jquery_scripts' );
 add_action('wp_footer', 'scripts_theme');
+add_action('before_footer', 'text_output');
 
 function style_theme() {
 	wp_enqueue_style('theme-css', get_template_directory_uri() . '/theme/css/theme.css');
@@ -25,4 +26,8 @@ function my_jquery_scripts() {
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
     wp_enqueue_script( 'jquery' );
-}    
+}
+
+function text_output() {
+	echo 'Произвольный текст';
+}
--- SergeyYakimov/index.php
+++ SergeyYakimov/index.php
@@ -1,44 +1,10 @@
-<!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
-<head>
+<?php get_header(); ?>
 
-   <!--- Basic Page Needs
-   ================================================== -->
-   <meta charset="utf-8">
-	<title>Тестовое задание</title>
-	<meta name="description" content="">
-	<meta name="author" content="">
+<h1>Главная страница</h1>
 
-   <!-- Mobile Specific Metas
-   ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<?php get_footer(); ?>
 
-   <!-- Favicons
-	================================================== -->
-	<link rel="shortcut icon" href="favicon.ico" >
 
-	<?php wp_head(); ?> 
 
-</head>
 
-<body>
-
-   <!-- Header
-   ================================================== -->
-   <header>
-
-   </header> <!-- Header End -->
-
-	<h1>Главная страница</h1>
-   <!-- footer
-   ================================================== -->
-   <footer>
-
-		<?php wp_footer(); ?> 
-   </footer> <!-- Footer End-->   
-
-</body>
-
-</html>
+   
