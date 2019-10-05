0826a1ce0392752976165102762c08a8baa2b826
SergeyYakimov <teriertis@yandex.ru>
2019-10-06 00:11:59 +0800

    Put the message before the footer on the screen

--- SergeyYakimov-child/functions.php
+++ SergeyYakimov-child/functions.php
@@ -1,20 +1,19 @@
 <?php
 
-function remove_scripts()
-{
+add_action('test_done', 'last_text_output');
+add_action( 'wp_footer', 'remove_scripts', 11 );
+add_action( 'wp_footer', 'scripts' );
+add_action( 'wp_ajax_click', 'alert_function' );
+add_action('wp_ajax_new_click', 'new_function');
+
+function remove_scripts() {
     wp_dequeue_script( 'theme-js' );
 }
-add_action( 'wp_footer', 'remove_scripts', 11 );
-
-add_action( 'wp_footer', 'scripts' );
 
 function scripts() {
-	//wp_enqueue_script('ajax-js', get_stylesheet_directory_uri() . '/js/ajax.js');
+	wp_enqueue_script('ajax-js', get_stylesheet_directory_uri() . '/js/ajax.js');
 	wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js');
 }
-
-//add_action( 'wp_ajax_click', 'alert_function' );
-add_action('wp_ajax_new_click', 'new_function');
 
 function new_function() {
 
@@ -25,9 +24,13 @@ function new_function() {
 	die;
 } 
  
-/*function alert_function() {
+function alert_function() {
 
 	echo 'WellYes World';
 
 	die;
-}*/
+}
+
+function last_text_output() {
+	echo 'Test Was Done!';
+}
